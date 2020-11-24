0f9665356987aa7c2d681256724d7831e7b8547a
Cesar Carmona <cesrafcata@gmail.com>
2020-11-23 22:06:47 -0700

    Añadidas funciones de la clase del 23-11-2020.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -74,4 +74,58 @@ public class Controller {
         Matcher matcher=patron.matcher(texto);
         lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
     }
+    public void pesosMexicanos(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^[$]([0-9]{1,})([.]?)([0.9]*).*[MXN|mxn]$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void tarjetaDeCredito(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("([0-9]{4})([-| ]?)([0-9]{4})([-| ]?)([0-9]{4})([-| ]?)([0-9]{4})");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void etiquetaHTML(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^<html(.*?)>(.*?)</html>$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void colorHexadecimal(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^[#]([0-9A-Fa-f]{1,8})");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void url(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^(http|https)://(?=.*(\\w\\.){1,}).*[\\w]$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void queryInsert(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^(INSERT INTO|INSERT into|insert INTO|Insert Into|Insert into|insert Into|insert into).*\\w.*(VALUES|values|Values).*([(]{1}).*([)]{1}).*([;]?)$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void ifDeVariable(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^if.*\\(.*\\w.*((==|<|>|>=|<=|!=)+).*\\w.*\\).*([{]{1})$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void fraseConNewline(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("\\w.*(?=\\\\n).*\\w");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
+    public void videoDeYouTube(ActionEvent event){
+        String texto=tfRegex.getText();
+        Pattern patron=Pattern.compile("^(http|https)://.*(?=(www.youtube.com){1}).*([/]*)((watch\\?v\\=){1}).*[\\w]$");
+        Matcher matcher=patron.matcher(texto);
+        lbResultado.setText((matcher.matches())?"Sí cumple":"No cumple");
+    }
 }
